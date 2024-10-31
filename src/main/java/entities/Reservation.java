f2f6e7443f04902105db72d02f4e432aa58c3c45
SouJalili <jalilisouad13@gmail.com>
2024-11-01 00:47:07 +0100

    Update Reservation.java

--- src/main/java/entities/Reservation.java
+++ src/main/java/entities/Reservation.java
@@ -3,6 +3,7 @@ import jakarta.persistence.*;
 import java.sql.Date;
 
 @Entity
+@Data
 public class Reservation {
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,49 +14,14 @@ public class Reservation {
 	private String passager_reservant;
 	@Column
 	private Date date_reservation;
-	@Column
-	private String status;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 16)
+	private ReservationStatut status;
 	@Column
 	private int prix_total;
-	public Reservation(String vol_reserve, String passager_reservant, Date date_reservation, String status,
-			int prix_total) {
-		this.vol_reserve = vol_reserve;
-		this.passager_reservant = passager_reservant;
-		this.date_reservation = date_reservation;
-		this.status = status;
-		this.prix_total = prix_total;
-	}
-	public String getVol_reserve() {
-		return vol_reserve;
-	}
-	public void setVol_reserve(String vol_reserve) {
-		this.vol_reserve = vol_reserve;
-	}
-	public String getPassager_reservant() {
-		return passager_reservant;
-	}
-	public void setPassager_reservant(String passager_reservant) {
-		this.passager_reservant = passager_reservant;
-	}
-	public Date getDate_reservation() {
-		return date_reservation;
-	}
-	public void setDate_reservation(Date date_reservation) {
-		this.date_reservation = date_reservation;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public int getPrix_total() {
-		return prix_total;
-	}
-	public void setPrix_total(int prix_total) {
-		this.prix_total = prix_total;
-	}
+	@ManyToOne
+	private Passager passager;
+	@ManyToOne
+	private Vol vol;
 	
-	
-
 }
