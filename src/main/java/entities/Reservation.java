f18d74184a491592628b93ac11d446a8a7172c4c
SouJalili <jalilisouad13@gmail.com>
2024-11-01 18:16:57 +0100

    Update Reservation.java

--- src/main/java/entities/Reservation.java
+++ src/main/java/entities/Reservation.java
@@ -5,23 +5,23 @@ import java.sql.Date;
 @Entity
 @Data
 public class Reservation {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	@Column
-	private String vol_reserve;
-	@Column
-	private String passager_reservant;
-	@Column
-	private Date date_reservation;
-    @Enumerated(EnumType.STRING)
-    @Column(length = 16)
-	private ReservationStatut status;
-	@Column
-	private int prix_total;
-	@ManyToOne
-	private Passager passager;
-	@ManyToOne
-	private Vol vol;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Column
+private String vol_reserve;
+@Column
+private String passager_reservant;
+@Column
+private Date date_reservation;
+@Enumerated(EnumType.STRING)
+@Column(length = 16)
+private ReservationStatut status;
+@Column
+private int prix_total;
+@ManyToOne
+private Passager passager;
+@ManyToOne
+private Vol vol;
 	
 }
