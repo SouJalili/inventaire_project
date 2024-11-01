9f9bb40a88ec28e9f908b848f3d09ed00d4ba6de
SouJalili <jalilisouad13@gmail.com>
2024-11-01 18:20:09 +0100

    Update Passager.java

--- src/main/java/entities/Passager.java
+++ src/main/java/entities/Passager.java
@@ -4,22 +4,21 @@ import jakarta.persistence.*;
 @Entity
 @Data
 public class Passager extends User{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-    @Column
-	private int num_passeport;
-    @Column
-	private String num_CIN;
-    @Column
-	private String nationalite;
-    @Column
-	private String adresse;
-    @Column
-	private double telephone;
-    @OneToMany(mappedBy = "passager")
-    private List<Reservation>reservation;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;	
+@Column
+private int num_passeport;
+@Column
+private String num_CIN;
+@Column
+private String nationalite;
+@Column
+private String adresse;
+@Column
+private double telephone;
+@OneToMany(mappedBy = "passager")
+private List<Reservation>reservation;
     
 	}
     
