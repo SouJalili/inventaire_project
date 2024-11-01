8865e7e9c3a0e7f1c529213bb95a30ad2a38989c
SouJalili <jalilisouad13@gmail.com>
2024-11-01 18:18:56 +0100

    Update Membre_equipage.java

--- src/main/java/entities/Membre_equipage.java
+++ src/main/java/entities/Membre_equipage.java
@@ -4,19 +4,18 @@ import jakarta.persistence.*;
 @Entity
 @Data
 public class Membre_equipage {
-    
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	@Column
-	private String nom_complet;
-	@Column
-	private String fonction;
-	@Column
-	private String num_licence;
-	@Column
-	private String nationalite;
-	@ManyToOne
-	private vol vol;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Column
+private String nom_complet;
+@Column
+private String fonction;
+@Column
+private String num_licence;
+@Column
+private String nationalite;
+@ManyToOne
+private vol vol;
 	
 }
