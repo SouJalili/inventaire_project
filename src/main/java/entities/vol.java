b8a5d38e32822fa2d1c820033a55b55ed55477ed
SouJalili <jalilisouad13@gmail.com>
2024-11-01 18:22:33 +0100

    Update vol.java

--- src/main/java/entities/vol.java
+++ src/main/java/entities/vol.java
@@ -4,21 +4,21 @@ import java.sql.Date;
 
 @Entity
 public class vol {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	@Column
-	private int num_vol;
-	@Column
-	private String vol_IATA;
-	@Column
-	private Date date_vol;
-	@Column
-	private String aeroport_depart;
-	@Column
-	private String aeroport_arrive;
-	@Column
-	@OneToMany(mappedBy = "vol")
-        private List<Membre_equipage>membreEquipage;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Column
+private int num_vol;
+@Column
+private String vol_IATA;
+@Column
+private Date date_vol;
+@Column
+private String aeroport_depart;
+@Column
+private String aeroport_arrive;
+@Column
+@OneToMany(mappedBy = "vol")
+private List<Membre_equipage>membreEquipage;
 	
 }
