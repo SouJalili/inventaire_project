efd95dd9ef69f7d3167d0aa72d62acf011c80452
SouJalili <jalilisouad13@gmail.com>
2024-11-01 18:18:07 +0100

    Update Aeoroport.java

--- src/main/java/entities/Aeoroport.java
+++ src/main/java/entities/Aeoroport.java
@@ -4,19 +4,19 @@ import jakarta.persistence.*;
 @Entity
 @Data
 public class Aeoroport {
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-	@Column
-	private String aeoroport_IATA;
-	@Column
-	private String nom_aeroport;
-	@Column
-	private String ville;
-	@Column
-	private String pays;
-	@Column
-	private int capacite;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+@Column
+private String aeoroport_IATA;
+@Column
+private String nom_aeroport;
+@Column
+private String ville;
+@Column
+private String pays;
+@Column
+private int capacite;
 	
 	
 }
